d020dde9dc096cf7a7591a712a2671ba5c55ed21
Alexander <98797176+bottomcity@users.noreply.github.com>
2023-01-04 11:03:38 +0700

    login-added_username

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  env : {
+    username : 'testsquid39testsquid39testsquid39testsq'
+  },
+
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'https://app.devsquid.net/',
+
+    chromeWebSecurity: false,
+
+    defaultCommandTimeout: 2000,
+
+    viewportWidth: 1280,
+    viewportHeight: 720
+
   },
 });
